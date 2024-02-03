58e35151a0e59738b07f77948ac623e4f95f8e1e
Bisra Batool <bisrabatool345@gmail.com>
2024-02-03 08:52:40 -0800

    landing page

--- Tempreature converter/app.js
+++ Tempreature converter/app.js
@@ -1,8 +1,8 @@
-// app.js
+
 
 function clearFields() {
-    document.getElementById('con1').value = ''; // Clear Celsius input
-    document.getElementById('con2').value = ''; // Clear Fahrenheit input
+    document.getElementById('con1').value = ''; 
+    document.getElementById('con2').value = ''; 
 }
 
 function updateTemperature(inputField) {
@@ -10,23 +10,23 @@ function updateTemperature(inputField) {
     var fahrenheitInput = document.getElementById('con2').value;
 
     if (inputField === 'con1' && celsiusInput !== '') {
-        // Convert Celsius to Fahrenheit
+        
         var celsiusValue = parseFloat(celsiusInput);
         var fahrenheitValue = (celsiusValue * 9/5) + 32;
         document.getElementById('con2').value = fahrenheitValue.toFixed(2);
     } else if (inputField === 'con2' && fahrenheitInput !== '') {
-        // Convert Fahrenheit to Celsius
+    
         var fahrenheitValue = parseFloat(fahrenheitInput);
         var celsiusValue = (fahrenheitValue - 32) * 5/9;
         document.getElementById('con1').value = celsiusValue.toFixed(2);
     } else {
-        // If the input field is cleared, update the other field accordingly
+        
         document.getElementById('con1').value = '';
         document.getElementById('con2').value = '';
     }
 }
 
-// Event listeners for automatic conversion
+
 document.getElementById('con1').addEventListener('input', function() {
     updateTemperature('con1');
 });
